f049e046e00169ec0b626410c2e4cebd537de2ed
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-06-07 13:06:08 -0500

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Mario Mendoza on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
